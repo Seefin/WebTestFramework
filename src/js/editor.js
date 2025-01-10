8626bb8e959d527fd82853bfdea89deddbf9d0ee
Connor Findlay <git@findlays.io>
2025-01-10 21:00:32 +1300

    Add Gui for steps
    Add test step interface that isn't just a JSON blob.

--- src/js/editor.js
+++ src/js/editor.js
@@ -4,6 +4,7 @@
  * @constant {Object} MESSAGES
  * @property {Function} ERR_EMPTY - Returns a message indicating that a value is required.
  * @property {Function} ERR_INVALID - Returns a message indicating that the provided JSON is invalid.
+ * @property {Function} ERR_NOFILE - Returns a message indicating that the user needs to select a file.
  * @property {Function} CONFIRM_DELETE_TEST - Returns a confirmation message for deleting a test.
  */
 const MESSAGES = {
@@ -13,23 +14,52 @@ const MESSAGES = {
     CONFIRM_DELETE_TEST: (test) => `Are you sure you want to delete test ${test}?`
 }
 
+/**
+ * IDs of various HTML elements used in the editor.
+ * 
+ * @constant {Object} ELEMENT_IDS
+ * @property {string} EDITOR_FORM - The ID of the editor form element.
+ * @property {string} EDITOR_LANDING - The ID of the editor landing element.
+ * @property {string} METADATA_FORM - The ID of the metadata form element.
+ * @property {string} POSTCONDITIONS_LIST - The ID of the postconditions list element.
+ * @property {string} PRECONDITIONS_LIST - The ID of the preconditions list element.
+ * @property {string} PROCEDURE_AUTHOR - The ID of the procedure author element.
+ * @property {string} PROCEDURE_DESCRIPTION - The ID of the procedure description element.
+ * @property {string} PROCEDURE_NAME - The ID of the procedure name element.
+ * @property {string} PROCEDURE_UPDATED - The ID of the procedure updated element.
+ * @property {string} STEP_DESCRIPTION - The ID of the step description element.
+ * @property {string} STEP_EDITOR - The ID of the step editor element.
+ * @property {string} STEP_FORM - The ID of the step form element.
+ * @property {string} STEP_LIST - The ID of the steps list element.
+ * @property {string} STEP_NAME - The ID of the step name element.
+ * @property {string} STEP_NUMBER - The ID of the step number element.
+ * @property {string} TEST_EDITOR - The ID of the test editor element.
+ * @property {string} TEST_FORM - The ID of the test form element.
+ * @property {string} TEST_LIST - The ID of the test list element.
+ * @property {string} TEST_NAME - The ID of the test name element.
+ * @property {string} TEST_STEPS - The ID of the test steps element.
+ */
 const ELEMENT_IDS = {
-    EDITOR_LANDING: 'editor-landing',
     EDITOR_FORM: 'editor-form',
-    PROCEDURE_NAME: 'procedure-name',
-    PROCEDURE_UPDATED: 'procedure-updated',
+    EDITOR_LANDING: 'editor-landing',
+    METADATA_FORM: 'metadata-form',
+    POSTCONDITIONS_LIST: 'postconditions-list',
+    PRECONDITIONS_LIST: 'preconditions-list',
     PROCEDURE_AUTHOR: 'procedure-author',
     PROCEDURE_DESCRIPTION: 'procedure-description',
-    TEST_LIST: 'test-list',
+    PROCEDURE_NAME: 'procedure-name',
+    PROCEDURE_UPDATED: 'procedure-updated',
+    STEP_DESCRIPTION: 'step-description',
+    STEP_EDITOR: 'step-editor',
+    STEP_FORM: 'step-form',
+    STEP_LIST: 'steps-list',
+    STEP_NAME: 'step-name',
+    STEP_NUMBER: 'step-number',
     TEST_EDITOR: 'test-editor',
     TEST_FORM: 'test-form',
+    TEST_LIST: 'test-list',
     TEST_NAME: 'test-name',
-    PRECONDITIONS_LIST: 'preconditions-list',
-    POSTCONDITIONS_LIST: 'postconditions-list',
-    TEST_STEPS: 'test-steps',
-    JSON_ERROR: 'json-error',
-    JSON_CONTENT: 'json-content',
-    METADATA_FORM: 'metadata-form'
+    TEST_STEPS: 'test-steps'
 }
 /**
  * Array of all tests in the current procedure.
@@ -37,10 +67,20 @@ const ELEMENT_IDS = {
  */
 let currentTests = [];
 /**
+ * Array of all steps in the current test.
+ * @type {Array}
+ */
+let currentSteps = [];
+/**
  * Index of the currently edited test.
  * @type {number}
  */
 let currentTestIndex = undefined;
+/**
+ * Index of the currently edited step.
+ * @type {number}
+ */
+let currentStepIndex = undefined;
 
 document.getElementById('create-new').addEventListener('click', () => {
     document.getElementById(ELEMENT_IDS.EDITOR_LANDING).classList.add('hidden');
@@ -89,22 +129,11 @@ document.getElementById('add-test').addEventListener('click', () => {
 document.getElementById('test-form').addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const testStepsJson = document.getElementById(ELEMENT_IDS.TEST_STEPS).value;
-    const validationResult = isValidJSON(testStepsJson);
-
-    if (!validationResult.valid) {
-        const errorMessage = document.getElementById(ELEMENT_IDS.JSON_ERROR);
-        errorMessage.innerText = `${MESSAGES.ERR_INVALID()}: ${validationResult.message}`;
-        errorMessage.classList.remove('hidden');
-        document.getElementById(ELEMENT_IDS.TEST_STEPS).classList.add('error');
-        highlightError(validationResult.line, validationResult.column, document.getElementById(ELEMENT_IDS.TEST_STEPS));
-        return;
-    }
     const test = {
         "Test Name": document.getElementById(ELEMENT_IDS.TEST_NAME).value,
         "Test Preconditions": Array.from(document.querySelectorAll('.precondition-input')).map(input => input.value).filter(Boolean),
         "Test Postconditions": Array.from(document.querySelectorAll('.postcondition-input')).map(input => input.value).filter(Boolean),
-        "Test Steps": JSON.parse(testStepsJson)
+        "Test Steps": currentSteps
     };
 
     if (currentTestIndex !== undefined) {
@@ -116,9 +145,7 @@ document.getElementById('test-form').addEventListener('submit', (e) => {
 
     document.getElementById(ELEMENT_IDS.EDITOR_FORM).classList.remove('hidden');
     document.getElementById(ELEMENT_IDS.TEST_EDITOR).classList.add('hidden');
-    document.getElementById(ELEMENT_IDS.TEST_STEPS).classList.remove('error');
-    document.getElementById(ELEMENT_IDS.JSON_ERROR).classList.add('hidden');
-
+    
     renderTests(document.getElementById(ELEMENT_IDS.TEST_LIST));
 });
 
@@ -151,6 +178,41 @@ document.getElementById('add-postcondition').addEventListener('click', () => {
     document.getElementById(ELEMENT_IDS.POSTCONDITIONS_LIST).appendChild(container);
 });
 
+document.getElementById(ELEMENT_IDS.STEP_FORM).addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const step = {
+        step: parseInt(document.getElementById(ELEMENT_IDS.STEP_NUMBER).value, 10),
+        stepName: document.getElementById(ELEMENT_IDS.STEP_NAME).value,
+        description: document.getElementById(ELEMENT_IDS.STEP_DESCRIPTION).value
+    };
+
+    if (currentStepIndex !== undefined) {
+        currentSteps[currentStepIndex] = step;
+        currentStepIndex = undefined;
+    } else {
+        currentSteps.push(step);
+    }
+
+    currentSteps.sort((a, b) => a.step - b.step);
+
+    document.getElementById(ELEMENT_IDS.STEP_EDITOR).classList.add('hidden');
+    document.getElementById(ELEMENT_IDS.TEST_EDITOR).classList.remove('hidden');
+    renderSteps();
+});
+
+document.getElementById('add-step').addEventListener('click', () => {
+    clearStepForm();
+    document.getElementById(ELEMENT_IDS.TEST_EDITOR).classList.add('hidden');
+    document.getElementById(ELEMENT_IDS.STEP_EDITOR).classList.remove('hidden');
+});
+
+document.getElementById('cancel-step').addEventListener('click', () => {
+    document.getElementById(ELEMENT_IDS.STEP_EDITOR).classList.add('hidden');
+    document.getElementById(ELEMENT_IDS.TEST_EDITOR).classList.remove('hidden');
+});
+
+// LOAD and STORE code
 function loadProcedure(procedure) {
     document.getElementById(ELEMENT_IDS.EDITOR_LANDING).classList.add('hidden');
     document.getElementById(ELEMENT_IDS.EDITOR_FORM).classList.remove('hidden');
@@ -163,12 +225,26 @@ function loadProcedure(procedure) {
     renderTests(document.getElementById(ELEMENT_IDS.TEST_LIST));
 }
 
+function storeProcedure() {
+    const procedure = {
+        "Test Procedure": document.getElementById(ELEMENT_IDS.PROCEDURE_NAME).value,
+        "Updated": document.getElementById(ELEMENT_IDS.PROCEDURE_UPDATED).value,
+        "Author": document.getElementById(ELEMENT_IDS.PROCEDURE_AUTHOR).value,
+        "Description": document.getElementById(ELEMENT_IDS.PROCEDURE_DESCRIPTION).value,
+        "Tests": currentTests
+    };
+    const blob = new Blob([JSON.stringify(procedure, null, 2)], { type: 'application/json' });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = `${procedure["Test Procedure"]}.json`;
+    a.click();
+    URL.revokeObjectURL(url);
+}
+
+// Clear Forms
 function clearProcedureForm() {
     document.getElementById(ELEMENT_IDS.METADATA_FORM).reset();
-    document.getElementById(ELEMENT_IDS.TEST_STEPS).value = '';
-    document.getElementById(ELEMENT_IDS.JSON_ERROR).classList.add('hidden');
-    document.getElementById(ELEMENT_IDS.JSON_ERROR).value = '';
-    document.getElementById(ELEMENT_IDS.TEST_STEPS).classList.remove('error');
 }
 
 function clearTestForm() {
@@ -189,28 +265,13 @@ function clearTestForm() {
             </div>
         </div>
     `;
-    document.getElementById(ELEMENT_IDS.TEST_STEPS).value = '';
-    document.getElementById(ELEMENT_IDS.JSON_ERROR).classList.add('hidden');
-    document.getElementById(ELEMENT_IDS.TEST_STEPS).classList.remove('error');
 }
 
-function storeProcedure() {
-    const procedure = {
-        "Test Procedure": document.getElementById(ELEMENT_IDS.PROCEDURE_NAME).value,
-        "Updated": document.getElementById(ELEMENT_IDS.PROCEDURE_UPDATED).value,
-        "Author": document.getElementById(ELEMENT_IDS.PROCEDURE_AUTHOR).value,
-        "Description": document.getElementById(ELEMENT_IDS.PROCEDURE_DESCRIPTION).value,
-        "Tests": currentTests
-    };
-    const blob = new Blob([JSON.stringify(procedure, null, 2)], { type: 'application/json' });
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = `${procedure["Test Procedure"]}.json`;
-    a.click();
-    URL.revokeObjectURL(url);
+function clearStepForm() {
+    document.getElementById('step-form').reset();
 }
 
+// Render Components
 function renderTests(testList) {
     if (!testList) {
         return;
@@ -227,10 +288,28 @@ function renderTests(testList) {
     `).join('');
 }
 
+function renderSteps() {
+    const stepsList = document.getElementById(ELEMENT_IDS.STEP_LIST);
+    stepsList.innerHTML = currentSteps.map((step, index) => `
+        <div class="step-card">
+            <div class="step-header">
+                <span class="step-title">Step ${step.step}: ${step.stepName}</span>
+                <div class="test-card-buttons">
+                    <button type="button" onclick="editStep(${index})" class="button">Edit</button>
+                    <button type="button" onclick="deleteStep(${index})" class="button">Delete</button>
+                </div>
+            </div>
+            <div class="step-description">${step.description}</div>
+        </div>
+    `).join('');
+}
+
+// Test CRUD code
 function editTest(index) {
     currentTestIndex = index;
     const test = currentTests[index];
     document.getElementById(ELEMENT_IDS.TEST_NAME).value = test["Test Name"];
+    currentSteps = test["Test Steps"];
 
     const preList = document.getElementById(ELEMENT_IDS.PRECONDITIONS_LIST);
     preList.innerHTML = test["Test Preconditions"]
@@ -256,7 +335,7 @@ function editTest(index) {
         `)
         .join('');
 
-    document.getElementById(ELEMENT_IDS.TEST_STEPS).value = JSON.stringify(test["Test Steps"], null, 2);
+    renderSteps();
     document.getElementById(ELEMENT_IDS.EDITOR_FORM).classList.add('hidden');
     document.getElementById(ELEMENT_IDS.TEST_EDITOR).classList.remove('hidden');
 }
@@ -275,32 +354,25 @@ function deleteTestCondition(button) {
     button.closest('.condition-row').remove();
 }
 
-function isValidJSON(jsonContent) {
-    try {
-        JSON.parse(jsonContent);
-        return { valid: true };
-    } catch (error) {
-        const lineMatch = error.message.match(/line (\d+)/i);
-        const columnMatch = error.message.match(/column (\d+)/i);
-        const line = lineMatch ? parseInt(lineMatch[1], 10) : null;
-        const column = columnMatch ? parseInt(columnMatch[1], 10) : null;
-        return {
-            valid: false,
-            message: error.message,
-            line: line,
-            column: column
-        };
-    }
+// Step CRUD code
+function editStep(index) {
+    currentStepIndex = index;
+    const step = currentSteps[index];
+    document.getElementById(ELEMENT_IDS.STEP_NUMBER).value = step.step;
+    document.getElementById(ELEMENT_IDS.STEP_NAME).value = step.stepName;
+    document.getElementById(ELEMENT_IDS.STEP_DESCRIPTION).value = step.description;
+    document.getElementById(ELEMENT_IDS.TEST_EDITOR).classList.add('hidden');
+    document.getElementById(ELEMENT_IDS.STEP_EDITOR).classList.remove('hidden');
 }
 
-function highlightError(line, column, textArea) {
-    const lines = textArea.value.split('\n');
-    let start = 0;
-    for (let i = 0; i < line - 1; i++) {
-        start += lines[i].length + 1;
+function deleteStep(index) {
+    console.debug(index);
+    if ( (!index && index !== 0) || index < 0 || index >= currentSteps.length) {
+        console.debug(currentSteps.length);
+        return;
     }
-    const end = start + column - 1;
-    textArea.focus();
-    textArea.setSelectionRange(end, end + 1);
-    textArea.scrollTop = textArea.scrollHeight;
+    if (confirm(`Are you sure you want to delete step ${currentSteps[index].stepName}?`)) {
+        currentSteps.splice(index, 1);
+        renderSteps();
+    }
 }
